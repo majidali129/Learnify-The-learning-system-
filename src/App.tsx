cca14fb12ac2e912bd0f11cd7866653dd9c6ad1b
Majid Ali <lahmajidali@gmail.com>
2024-03-25 16:57:13 +0500

    added possible pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl text-red-400 font-bold underline">
       Hello world!
       <Button>Click me</Button>
     </h1>
